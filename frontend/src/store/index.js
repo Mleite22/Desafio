6ca5d81490b6839a93eb3bb542516a3aa2eeee0f
Mleite22 <leitelopesmarcos@gmail.com>
2024-10-08 15:06:30 -0400

    Instalando o front end

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,14 +1,59 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
-  },
-  getters: {
+    alunos: [],
+    cursos: []
   },
   mutations: {
+    SET_ALUNOS(state, alunos) {
+      state.alunos = alunos;
+    },
+    SET_CURSOS(state, cursos) {
+      state.cursos = cursos;
+    }
   },
   actions: {
+    fetchAlunos({ commit }) {
+      axios.get('http://localhost:8000/api/users')
+        .then(response => {
+          commit('SET_ALUNOS', response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    fetchCursos({ commit }) {
+      axios.get('http://localhost:8000/api/cursos')
+        .then(response => {
+          commit('SET_CURSOS', response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    cadastrarAluno({ dispatch }, aluno) {
+      axios.post('http://localhost:8000/api/users', aluno)
+        .then(() => {
+          dispatch('fetchAlunos');
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+    cadastrarCurso({ dispatch }, curso) {
+      axios.post('http://localhost:8000/api/cursos', curso)
+        .then(() => {
+          dispatch('fetchCursos');
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   },
-  modules: {
+  getters: {
+    alunos: state => state.alunos,
+    cursos: state => state.cursos
   }
-})
+});
