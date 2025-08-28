c99ac64fb2f118de5f2b536428df9115cd51269d
Mleite22 <leitelopesmarcos@gmail.com>
2025-08-28 13:59:30 -0400

    Ajustes no aditarPerfil.ts

--- front-office/src/services/editar_perfil/editarPerfilUser.ts
+++ front-office/src/services/editar_perfil/editarPerfilUser.ts
@@ -1,7 +1,14 @@
 import createAxiosInstance from "@/utils/axios";
 
+interface UserProfile {
+  id: number;
+  name: string;
+  email: string;
+  password?: string;  // Senha opcional
+  
+}
 
-async function editarPerfilUser(userData) {
+async function editarPerfilUser(userData: UserProfile) {
   const axiosInstance = createAxiosInstance();
   try {
     const response = await axiosInstance.patch("/users/profile", userData);
